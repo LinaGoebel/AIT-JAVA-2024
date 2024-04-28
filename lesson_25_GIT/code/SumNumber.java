27800b5de802ab3c66053e578c872fd75f18c2f0
Lina Göbel <galina283@freenet.de>
2024-04-28 17:00:38 +0200

    add lesson 25

---
+++ lesson_25_GIT/code/SumNumber.java
@@ -0,0 +1,14 @@
+public class SumNumber {
+
+  public static void main(String[] args) {
+    int y = 1979;
+    int i = 1;
+    int sum = 0;
+
+    while (i <= y) {
+      sum += i;
+      i++;
+    }
+    System.out.println(sum);
+  }
+}
